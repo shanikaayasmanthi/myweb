69cd19c098fb09da541171951e0399616b0ed3ef
shanikaayasmanthi <2021cs016@stu.ucsc.cmb.ac.lk>
2024-05-17 16:10:52 +0530

    create home

---
+++ src/components/Aboutme.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './aboutme.css'
+import Profile from'./prifile.png'
+// import Prifile from './prifile.jpg'
+
+export default function Aboutme() {
+  return (
+    <div>
+      
+    </div>
+    
+  )
+}
