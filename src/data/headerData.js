c45c7aa3308d146899e76c99c4639123c4e5f19d
P.S.Ayasmanthi <111691675+shanikaayasmanthi@users.noreply.github.com>
2025-09-29 17:37:52 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import profile from '../assets/images/prifile.png'
 
 export const headerData = {
     name: 'SHANIKA AYASMANTHI',
-    title: "FSD",
+    title: "Software Engineer",
     desciption:"Life may be a meandering path but don't worry about it, rather enjoy it. It is about the journey, not the destination so explore all that comes your way, learn and grow from it, for this journey only happens once.",
     image: profile,
     resumePdf: resume,
